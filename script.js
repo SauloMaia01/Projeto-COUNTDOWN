84bac7e7f3f6bb2bacbc196a764d658a4fe7bbfe
SauloMaia01 <saulormaia@hotmail.com>
2022-06-14 11:52:55 -0300

    feat(function formatarDigito): Creation of the function formatarDigito
    Creation of the function formatarDigito where it removes a zero from the left when it has 3 digits

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const atualizar = tempo) => {
+const formatarDigito = (digito) => `0${digito}`.slice(-2);
+/* slice corda o digito da esquerda para a direita, se por negativo corta a direita para a esquerda */
+
+const atualizar = (tempo) => {
     const segundos = document.getElementById('segundos');
 
     segundos.textContent = (tempo);
@@ -22,4 +25,4 @@ const contagemRegressiva = (tempo) => {
     const id = setInterval(contar, 1000);
 }
 
-contagemRegressiva(5);
+contagemRegressiva(12);
